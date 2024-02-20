2a40896355bb3a747ae6c397087b407381ca584c
A-L-A <64477195+A-L-A@users.noreply.github.com>
2024-02-20 19:16:49 +0400

    ..

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,17 +1,22 @@
+import { get } from "mongoose";
 import data from "./data";
 import { useState } from "react";
 
 export default function Accordion() {
-	
+
   const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId){
+	console.log(getCurrentId);
+  }
 
   return (
     <div className="wrapper">
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem, index) => (
-            <div className="item" key={index}>
-              <div className="title">
+            <div className="item">
+              <div onlick={()=>handleSingleSelection(dataItem.id)} className="title">
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
